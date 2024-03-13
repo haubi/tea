84a543bdc2f9101aee8e0264ff05a7617c37490b
Daniel Pressnitz <daniel.pressnitz@ssi-schaefer.com>
2024-03-13 14:41:56 +0100

    smaller fixes
    * added icon for "Others" menu
    * added description for TaskChainCleanProjectsWithErrors

--- bundles/org.eclipse.tea.library.build/src/org/eclipse/tea/library/build/tasks/chains/TaskChainCleanProjectsWithErrors.java
+++ bundles/org.eclipse.tea.library.build/src/org/eclipse/tea/library/build/tasks/chains/TaskChainCleanProjectsWithErrors.java
@@ -14,11 +14,13 @@ import org.eclipse.tea.core.TaskExecutionContext;
 import org.eclipse.tea.core.annotations.TaskChainContextInit;
 import org.eclipse.tea.core.annotations.TaskChainMenuEntry;
 import org.eclipse.tea.core.services.TaskChain;
+import org.eclipse.tea.core.services.TaskChain.TaskChainId;
 import org.eclipse.tea.library.build.menu.BuildLibraryMenuDecoration;
 import org.eclipse.tea.library.build.menu.OtherMenuDecoration;
 import org.eclipse.tea.library.build.tasks.TaskCleanProjectsWithErrors;
 import org.osgi.service.component.annotations.Component;
 
+@TaskChainId(description = "Clean Projects with Errors")
 @TaskChainMenuEntry(path = { OtherMenuDecoration.MENU_OTHER,
 		BuildLibraryMenuDecoration.MENU_BUILD }, groupingId = BuildLibraryMenuDecoration.GROUP_MISC, icon = BuildLibraryMenuDecoration.ICON_BUILD)
 @Component
